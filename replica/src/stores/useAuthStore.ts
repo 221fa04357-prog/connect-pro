fa5e9fc903192791a6302ce7a09c7278f14effd5
sravyavajja05 <sravyavajja3@gmail.com>
2026-01-31 17:07:36 +0530

    WIP: saving local changes before branching

--- replica/src/stores/useAuthStore.ts
+++ replica/src/stores/useAuthStore.ts
@@ -1,11 +1,5 @@
 import { create } from 'zustand';
-
-interface User {
-    id: string;
-    name: string;
-    email: string;
-    subscriptionPlan?: 'free' | 'pro' | 'enterprise';
-}
+import { User } from '@/types';
 
 interface AuthState {
     user: User | null;
@@ -14,7 +8,7 @@ interface AuthState {
     login: (user: User) => void;
     logout: () => void;
     setSubscription: (plan: User['subscriptionPlan']) => void;
-}
+} 
 
 // Helpers for localStorage
 const AUTH_KEY = 'connectpro_auth';
